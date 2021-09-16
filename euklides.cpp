6e9a4a6686120b7cc3a1ea935f9bbe9a8d2cb18f
gucio321 <gucio321@protonmail.com>
2021-09-16 21:40:18 +0200

    initial commit

---
+++ euklides.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+
+using namespace std;
+
+int input(const char* value) {
+        cout << value;
+        int output;
+        cin >> output;
+        return output;
+}
+
+int euklidesAlghoritm(int a, int b) {
+        while (a != b) {
+                if (a > b) {
+                        a = a - b;
+                } else {
+                        b = b - a;
+                }
+        }
+
+        // when the numbers are equal, GCD (polish: NWD) is: GCD = a = b
+        return a;
+}
+
+int main() {
+        int a = input("enter a ");
+        int b = input("enter b ");
+
+        int gcd = euklidesAlghoritm(a, b);
+
+        printf("GCD of numbers %d, %d = %d", a, b, gcd);
+
+	return 0;
+}
